e3678d9c4d07634d148effbdb345a60ed69bc340
TheNush <40197429+TheNush@users.noreply.github.com>
2019-11-23 15:21:32 +0530

    Add files via upload

--- automate.py
+++ automate.py
@@ -4,16 +4,28 @@ import pywt
 import matplotlib.pyplot as plt
 
 # Prepare data
-runoff = pd.read_csv('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Navalgund.csv', header=0)
+runoff = pd.read_csv('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Cholachguda.csv', header=0)
 # Remove last 7 points so as to perform SWT
-runoff = np.array(runoff[:-7])
+length = (len(runoff)//32) * 32
+runoff = np.array(runoff[:length])
 # coeffs = np.array(pywt.swt(runoff, 'db2', level=3, axis=0))
 
 wavelets = ['db', 'haar', 'coif', 'sym']
 
-def decompose(wavelet):
-	for j in range(2,8):
-		wlt = wavelet + str(j)
+wavelets_dict = {}
+wavelets_dict[wavelets[0]] = [2,8]
+wavelets_dict[wavelets[1]] = [1,2]
+wavelets_dict[wavelets[2]] = [1,6]
+wavelets_dict[wavelets[3]] = [2,8]
+
+# print(wavelets_dict[wavelets[0]])
+
+def decompose(wavelet, mems):
+	for j in range(mems[0],mems[1]):
+		if wavelet == 'haar':
+			wlt = wavelet
+		else:
+			wlt = wavelet + str(j)
 		# print(wavelet)
 		for i in range(1,6):
 			coeffs = np.array(pywt.swt(runoff, wlt, level=i, axis=0))
@@ -22,9 +34,9 @@ def decompose(wavelet):
 			details = details.transpose()
 			# print(details.shape)
 			print(wlt)
+			approxs.to_csv('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Decomps/Cholachguda/L{}/approxs-{}_2.csv'.format(i, wlt), index=False)
+			details.to_csv('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Decomps/Cholachguda/L{}/details-{}_2.csv'.format(i, wlt), index=False)
 
-			approxs.to_csv('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Decomps/Navalgund/L{}/approxs-{}.csv'.format(i, wlt), index=False)
-			details.to_csv('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Decomps/Navalgund/L{}/details-{}.csv'.format(i, wlt), index=False)
-
-# run decompose function for different wavelet families
-decompose(wavelets[3])
+for i in range(0, 4):
+	decompose(wavelets[i], wavelets_dict[wavelets[i]])
+	print('Decomposition of {}'.format(wavelets[i]))
--- mat.py
+++ mat.py
@@ -6,26 +6,36 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
-# from statsmodels.tsa.arima_model import ARIMA
 
-discharges = pd.read_csv("C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Khanapur.csv", header=0)
-discharges = discharges[:-5]
-
-print(len(discharges))
+def adjust(df):
+	length = (len(df)//32) * 32
+	df = df[:length]
+	return df
 
 def normalize(x):
 	return((x-np.min(x))/(np.max(x) - np.min(x)))
+
+discharges = pd.read_csv("C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Navalgund.csv", header=0)
+discharges = adjust(discharges)
+print(len(discharges))
+
+print(len(discharges))
  
 for j in range(1,6):
-	for i in range(1,6):
+	for i in range(2,7):
 		# i = ""
-		details = pd.read_csv("C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Decomps/Khanapur/L{}/details-coif{}.csv".format(j, i), header=0)
-		approx = pd.read_csv("C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Decomps/Khanapur/L{}/approxs-coif{}.csv".format(j, i), header=0)
+		details = pd.read_csv("C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Decomps/Navalgund/L{}/details-db{}.csv".format(j, i), header=0)
+		details = adjust(details)
+		approx = pd.read_csv("C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Datasets/Decomps/Navalgund/L{}/approxs-db{}.csv".format(j, i), header=0)
+		approx = adjust(approx)
 		y_train = (discharges.iloc[1:, 0])
 		x_train = normalize((pd.concat([approx, details], axis=1)[:-1]))
+		print(x_train.shape)
 		x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25, shuffle=False)
 		y_val = y_val.reset_index(drop=True)
-		model = svm.SVR(kernel='sigmoid', C=800, epsilon=0.01, gamma=0.1, verbose=1)
+		model = svm.SVR(kernel='rbf', C=8000, epsilon=1, gamma=3, verbose=1)	# Navalgund
+		# model = svm.SVR(kernel='sigmoid', C=800, epsilon=0.01, gamma=0.1, verbose=1)	# Khanapur
+		# model = svm.SVR(kernel='sigmoid', C=800, epsilon=0.01, gamma=0.1, verbose=1) # Cholachguda
 		model.fit((x_train), (y_train))
 		preds_test = model.predict(x_val)
 		preds_train = model.predict(x_train)
@@ -44,25 +54,27 @@ for j in range(1,6):
 		fig, axs = plt.subplots(1)
 		axs.plot(y_val, label="Actual Discharge")
 		axs.plot(preds_test, label='Forecasted Discharge')
+		axs.set_title('db{} Level {}'.format(i,j))
 		axs.set_xlabel("TIME(Day)")
 		axs.set_ylabel("Discharge(m^3/s)")
 		axs.legend(('Actual Discharge', 'Forecast Discharge'), fontsize=12)
-		fig.savefig('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Results/Khanapur/Coif/Line/L{}-coif{}.png'.format(j, i))
+		# fig.savefig('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Results/Cholachguda/db/Line/L{}-db{}_2.png'.format(j, i))
 
 		fig, axs = plt.subplots(1)
-		axs.scatter(y_val, preds_test, label='Forecasted v/s Actual')
+		axs.scatter(y_val, preds_test, label='Forecasted v/s True')
 		b, m = np.polynomial.polynomial.polyfit(y_val, preds_test, 1)
 		print("Slope: ", m)
 		print("Intercept: ", b)
 		axs.plot(y_val, m * y_val + b, color='black', label='y = {0:.2f}x + {0:.2f}'.format(m, b))
+		axs.set_title('db{} Level {}'.format(i,j))
 		axs.set_yticks([0, 100, 200, 300, 400, 500, 600, 700], minor=False)
 		axs.set_ylabel(r'Forecasted Discharge(m^3/s)')
 		axs.set_xlabel(r'Actual Discharge(m^3/s)')
 		axs.legend(('y = %0.2fx + %0.2f \nR Squared Score: %0.2f' % (m, b, r2_test), 'Forecasted v/s Actual'), fontsize=12)
 		# axs.text(0.2, 0.95, r'Regression Line: y = {0:.2f}x + {0:.2f}/nR Squared Score: {0:.2f}'.format(m, b, r2_test), fontsize=10,ha='center' , va='top', bbox={'facecolor': 'white', 'alpha': 0.5, 'pad': 10}, transform=axs.transAxes)
 		# axs.legend(('Forecasted v/s Actual', 'Regression Line: y = {0:.2f}x + {0:.2f}', 'R Squared Score: {0:.2f}'.format(m, b, r2_test),), 'best')
-		fig.savefig('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Results/Khanapur/Coif/Scatter/L{}-coif{}.png'.format(j, i))
-		plt.close('all')
-		# plt.show()
+		# fig.savefig('C:/Users/TheNush07/Desktop/Work/Projects/StreamFlow Forecasting/Results/Cholachguda/db/Scatter/L{}-db{}_2.png'.format(j, i))
+		# plt.close('all')
+		plt.show()
 		# print("/n")
-		print("Completed - L{} coif{}".format(j, i))
+		print("Completed - L{} db{}".format(j, i))
